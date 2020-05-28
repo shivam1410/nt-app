8d01b7bd3da4423e614799740dea5175ab981d7a
Shubhendra29 <66065309+Shubhendra29@users.noreply.github.com>
2020-05-28 18:53:20 +0400

    updated app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,30 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { AngularFireStorage } from 'angularfire2/storage';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
-})
+  styleUrls: ['app.component.scss'] ,
+  template:
+  <label for="file">File:</label>
+  <input type="file" (change)="upload($event)" accept=".png,.jpg" />
+    })
 export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private afStorage: AngularFireStorage
   ) {
+    upload(event) {
+    const randomId = Math.random().toString(36).substring(2);        
+    this.ref = this.afStorage.ref(randomId);
+    this.task = this.ref.put(event.target.files[0]);
+    this.uploadProgress = this.task.percentageChanges();
+    this.uploadState = this.task.snapshotChanges().pipe(map(s => s.state));
+    }
     this.initializeApp();
   }
 
